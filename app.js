4d46a0217f25e2c4cd17c5c041f3d2ff45a15e0b
Timothy Coleman <Timbo97@yahoo.com>
2023-12-28 11:02:10 -0500

    complete

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('searchWord');
+    const submitButton = document.getElementById('submitSearch');
+    const imageElement = document.querySelector('#imageContainer img');
+  
+    submitButton.addEventListener('click', () => {
+      const apiKey = 'xgsTJW5PNOP68caIOzgfHNAcaZ5APTvk'; 
+      const searchTerm = searchInput.value.trim();
+  
+      if (searchTerm !== '') {
+        const url = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchTerm}`;
+  
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            
+            imageElement.src = data.data.images.original.url;
+          })
+          .catch(error => {
+            console.error(error);
+          
+          });
+      } else {
+        console.log('Please enter a search term.');
+       
+      }
+    });
+  });
